045dcf99bbeb35c2ac988bd9846a4881db8be6bf
Rafael Tanaka <116230287+Zephele@users.noreply.github.com>
2022-10-20 01:20:07 +0000

    Nome-idade como colocar com frases

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ idade = 18
 # Exibir o meu nome (que está dentro da variavel nome)
 print (nome)
 
+# Quando quiser exibir a frase "Minha idade é " completando com o conteudo da variavel idade
+print ("Minha idade é "+str(idade))
+# Ou ----- f = formatação ---- se quiser pular linha (\n ou +n)
+print ( f"Minha idade é {idade}")
+# Ou
+print ("Minha idade é {}".format(idade))
 
+# Quando quiser exibir "Meu nome é ... e tenho.." trocando pelas variaveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
 
 '''
 Comentario em bloco
