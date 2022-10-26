f7c0c3c14f24c7bca72e0e7c9551c21cd7efe102
Rafael Tanaka <116230287+Zephele@users.noreply.github.com>
2022-10-26 23:14:34 +0000

    Ensinei comando de entrada input() e falamos novamente sobre a diferença de string para int....

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-# Meu primeiro projeto Python!!!
-#
-# print() = comando de saida
-print ("hello world!")
+# comando input(): quero permitir que o usuario digite algo...
+nome = input("Digite seu nome: ")
 
-# Quando quiser guardar uma String! (frase)
-nome = "Rafael Tanaka"
-# Quando quiser guardae um numero inteiro
-idade = 18
-
-# Exibir o meu nome (que está dentro da variavel nome)
+#comando de saida... exibir na tela
 print (nome)
+print (f"Boa noite, seu nome é {nome}")
 
-# Quando quiser exibir a frase "Minha idade é " completando com o conteudo da variavel idade
-print ("Minha idade é "+str(idade))
-# Ou ----- f = formatação ---- se quiser pular linha (\n ou +n)
-print ( f"Minha idade é {idade}")
-# Ou
-print ("Minha idade é {}".format(idade))
+idade = int(input("Qual a sua idade: "))
+print (f"Sua idade é {idade}")
 
-# Quando quiser exibir "Meu nome é ... e tenho.." trocando pelas variaveis nome e idade
-print("Meu nome é {} e tenho {} anos".format(nome,idade))
+#e se eu quisesse mostrar o DOBRO da idade informada?
 
-'''
-Comentario em bloco
-
-'''
+dobro = idade * 2
+print ("O dobro da idade informada é {}".format (dobro))
